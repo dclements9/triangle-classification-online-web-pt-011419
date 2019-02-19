0dc17c42b3c74a4b55ba65ea9e0d5e4906f01168
Dylan Clements <dclements@protonmail.com>
2019-02-19 13:27:37 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -5,6 +5,16 @@ class Triangle
     @side_a = side_a
     @side_b = side_b
     @side_c = side_c
+  end
+  def kind
+    if @side_a == @side_b && @side_b == @side_c
+      :equilateral
+    elsif @side_a == @side_b || @side_b == @side_c || @side_c == @side_a
+      :isosceles
+    else
+      :scalene
+  end
+  class TriangleError < StandardError
 
   end
 end
