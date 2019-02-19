1f53158eda0e39de5e6c91eecc29cb3560b46d68
Dylan Clements <dclements@protonmail.com>
2019-02-19 13:35:13 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,7 +7,7 @@ class Triangle
     @side_c = side_c
   end
   def kind
-    if @side_a <1 || @side_b <1 || @side_c <1 || (@side_a + @side_b) <= @side_c || (@side_a+ @side_c) <= @side_b || (@side_b+@side_c) <= @side_a
+    if @side_a <= 0 || @side_b <= 0 || @side_c <= 0 || (@side_a + @side_b) <= @side_c || (@side_a+ @side_c) <= @side_b || (@side_b+@side_c) <= @side_a
       raise TriangleError
     elsif @side_a == @side_b && @side_b == @side_c
       :equilateral
